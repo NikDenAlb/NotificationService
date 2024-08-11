05daca2e1c7c854a017872446a12720edc016d3e
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-08-11 15:15:49 +0300

    Initial commit

---
+++ src/test/java/com/k70/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k70.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
