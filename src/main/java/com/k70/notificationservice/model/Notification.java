5b28880810b9dc65d4ad77a92506d1cb26a725d2
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-08-11 20:12:36 +0300

    model Notification

---
+++ src/main/java/com/k70/notificationservice/model/Notification.java
@@ -0,0 +1,31 @@
+package com.k70.notificationservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+//toDel
+/**
+ * <pre>
+ * "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+ *
+ * "customerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+ *
+ * "message": "string",
+ *
+ * "sentAt": "2024-08-11T15:24:12.651Z"
+ * <pre/>
+ **/
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private UUID customerId;
+    private String message;
+    private LocalDateTime sentAt;
+}
